604dde92980bf17362daa9d87779c057210f9d21
Aydin <112811545+aydinhafiz@users.noreply.github.com>
2024-10-26 13:42:57 +0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../App.css";
 
 
-
 import Content from "../components/home-content/content";
 import Navbar from "../components/navbar/navbar";
 import Header from "../components/home-header/header";
